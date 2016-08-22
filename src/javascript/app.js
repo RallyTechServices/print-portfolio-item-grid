4fdd046a1e33939b4c327f1fc9c9debd93011349
John Martin <curmudgeon+git@rallydev.com>
2016-08-22 15:37:46 -0700

    add a default button to the field picker so admin can choose default settings

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -8,7 +8,7 @@ Ext.define("PPIC", {
         defaultSettings: {
             showScopeSelector: true,
             selectorType: null,
-            columnNames: ['FormattedID','Name'],
+            defaultColumnNames: ['FormattedID','Name'],
             showControls: true
         }
     },
@@ -272,9 +272,10 @@ Ext.define("PPIC", {
             context: this.getContext(),
             stateful: false,
             plugins:[{
-                ptype: 'rallygridboardfieldpicker',
+                ptype: 'tsgridboardfieldpicker',
                 headerPosition: 'left',
-                margin: '3 0 0 10'
+                margin: '3 0 0 10',
+                defaultFields: this.getSetting('defaultColumnNames')
             },
             {
                 ptype: 'rallygridboardinlinefiltercontrol',
@@ -310,6 +311,14 @@ Ext.define("PPIC", {
                     items: [{
                         xtype: 'tsbulkrecordmenuitem'
                     }]
+                },
+                listeners: {
+                    columnschanged: function(g) {
+                        console.log('columnschanged');
+                    },
+                    reconfigure: function(g) {
+                        console.log('reconfigure');
+                    }
                 }
             }
         });
@@ -362,7 +371,8 @@ Ext.define("PPIC", {
     },
 
     _getColumns: function() {
-        return this._getAlwaysSelectedFields();
+        return [];
+        //return this._getAlwaysSelectedFields();
     },
     
     _shouldEnableAddNew: function() {
